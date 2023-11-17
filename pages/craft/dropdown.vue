b8b2a7cd04be6429eec73eaca25bba9ad323e718
edezuraud <eddy.dezuraud@gmail.com>
2023-11-17 09:57:07 +0100

    Add dropdown craft

--- pages/craft/dropdown.vue
+++ pages/craft/dropdown.vue
@@ -1,138 +1,86 @@
 <script setup>
-import codes from '~/assets/data/codes/space-button.js';
+import codes from '~/assets/data/codes/dropdown.js';
 
 useHead({
     title: "Eddy's Playground : Dropdown"
 })
 
-const height = ref(0);
-const transform = ref('translateY(0)');
-const color = ref('currentColor');
-const opacity = ref(0);
-
-const itemColor = computed((item) => {
-    if (item.color) return { color: item.color }
-    return {}
-})
-
-const onMouseEnter = ($event, item) => {
-    if (item.color) {
-        color.value = item.color;
-    } else {
-        color.value = 'currentColor';
+const dropDownList = ref([
+    {
+        label: 'Unfollow',
+        id: 'unfollow',
+        icon: 'user-x',
+        color: '#79798a'
+    },
+    {
+        topSeparator: true,
+        label: 'Comment this post',
+        id: 'comment',
+        icon: 'message-2'
+    },
+    {
+        label: 'Embed',
+        id: 'embed',
+        icon: 'code'
+    },
+    {
+        label: 'Add to Lists',
+        id: 'addFromList',
+        icon: 'circle-plus'
+    },
+    {
+        topSeparator: true,
+        label: 'Mute @meta_user',
+        id: 'mute',
+        icon: 'volume-off'
+    },
+    {
+        label: 'Block @meta_user',
+        id: 'block',
+        icon: 'lock-access-off'
+    },
+    {
+        topSeparator: true,
+        label: 'Report',
+        id: 'report',
+        icon: 'hand-stop',
+        color: '#ff333a'
     }
-    transform.value = `translateY(${$event.currentTarget.offsetTop}px)`;
-    height.value = $event.currentTarget.offsetHeight + 'px';
-    opacity.value = 0.1;
-}
-
-const onMouseLeave = () => {
-    opacity.value = 0;
-}
+]);
 
 </script>
 
+
 <template>
-    <div :class="[$style.wrapper, { [$style.dark]: dark }]">
-        <div :class="$style.inner" @mouseleave="onMouseLeave">
-            <ul :class="$style.list">
-                <li v-for="(item, index) in list" :key="index" :style="itemColor(item)"
-                    :class="[$style.item, { [$style.topSeparator]: item.topSeparator }]" @click="$emit('click', item.id)"
-                    @mouseenter="onMouseEnter($event, item)">
-                    <component :is="item.icon + '-icon'" :class="$style.icon" />
-                    <span>{{ item.label }}</span>
-                </li>
-            </ul>
-            <div :class="$style.highlighter" :style="highlighter"></div>
+    <div :class="$style.wrapper">
+        <div class="global" :class="$style.inner">
+            <Dropdown :list="dropDownList" @click="onClickItem" />
+            <Dropdown :list="dropDownList" @click="onClickItem" dark />
         </div>
+        <CraftCode :codes="codes" />
     </div>
 </template>
 
 <style module>
 .wrapper {
-    background: white;
-    color: black;
-    border-radius: 10px;
-    padding: 10px;
-    width: 280px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 20px;
+    position: relative;
+    margin-top: 200px;
 }
 
 .inner {
-    position: relative;
-}
-
-.item {
     display: flex;
     align-items: center;
-    gap: 10px;
-    padding: 5px 8px;
-    font-weight: 600;
-    font-size: 14px;
-    border-radius: 6px;
+    justify-content: center;
     position: relative;
-    z-index: 2;
-    cursor: pointer;
-    transform: translateZ(0);
-    user-select: none;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-}
-
-.topSeparator {
-    margin-top: 5px;
-
-    &::before {
-        position: absolute;
-        content: '';
-        top: -3px;
-        left: 0;
-        width: 100%;
-        height: 1px;
-        background: #EBEEF1;
-    }
-}
-
-.icon {
-    font-size: 18px;
-}
-
-.highlighter {
-    position: absolute;
-    top: 0;
-    width: 100%;
-    background: currentColor;
-    opacity: 0.1;
-    border-radius: 6px;
-    z-index: 0;
-    transition: transform 0.15s, opacity 0.15s;
-}
-
-.list {
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    gap: 1px;
-}
-
-
-.dark {
-    background: rgb(19, 19, 21);
-    color: white;
-    box-shadow: rgb(0 0 0 / 12%) 0px 12px 12px, 0 0 0 1px rgba(255, 255, 255, 0.12);
-
-    .item {
-        font-weight: 500;
-    }
-
-    .topSeparator ::before {
-        position: absolute;
-        content: '';
-        top: -3px;
-        left: 0;
-        width: 100%;
-        height: 1px;
-        background: #2c2f31;
-    }
+    padding: 80px 20px;
+    border-radius: 12px;
+    background: hsla(0, 0%, 0%, 0.2);
+    border: solid 1px rgba(255, 255, 255, 0.05);
+    gap: 20px;
 }
 </style>
